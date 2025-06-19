a5d1932de510262db0d7d9b8ecb9bcf944741c60
Thuang6413 <Thuang6413@conestogac.on.ca>
2025-06-18 20:24:11 -0400

    fix the model using emotion_face_fer2013 for tesing

--- dev/backend/config.py
+++ dev/backend/config.py
@@ -6,8 +6,8 @@ class Config:
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
     MODEL_PATHS = {
-        "emotion_face_affectnet": os.path.join(BASE_DIR, "ai_models", "Emotion_Recognition_Model.h5"),
-        # "emotion_face_fer2013": os.path.join(BASE_DIR, "ai_models", "fer2013_emotion_recognition_model.h5")
+        # "emotion_face_affectnet": os.path.join(BASE_DIR, "ai_models", "Emotion_Recognition_Model.h5"),
+        "emotion_face_fer2013": os.path.join(BASE_DIR, "ai_models", "fer2013_emotion_recognition_model.h5")
     }
 
     # Database Configuration
